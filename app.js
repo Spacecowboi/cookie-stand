c690cb01a44f4401b9f2cdf574b9cb87a14e6886
spacecowboi <cunningham2192@gmail.com>
2020-01-14 09:39:31 -0800

    fixed auto-spacing problems, changed font style

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
+'use strict';
 //Open hours
-//Collaborated with Alex pena 
-var hours = [‘6am’,‘7am’,‘8am’,‘9am’,‘10am’,‘11am’,‘12pm’,‘1pm’,‘2pm’,‘3pm’,‘4pm’,‘5pm’,‘6pm’,‘7pm’];
+
+//Collaborated with Alex pena
+var hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
 //RNGESUS//
 function getRandomInt(customerMin, customerMax) {
-    return Math.floor(Math.random() * (customerMax - customerMin)) + customerMin;
+  return Math.floor(Math.random() * (customerMax - customerMin)) + customerMin;
 }
 console.log(getRandomInt(23,65));
 
 //store object//
 seattleStore = {
-    store: ‘Seattle’,
-    customerMin: 23,
-    customerMax: 65,
-    customerAverage: 6.3,
-//Customer/sales per hour//
-hourlySales: function(){
-var customersPerHour = [];
-var salesPerCustomer = [];
-    for (i=0; i<hours.length; i++){
-        customersPerHour.push(getRandomInt(this.customerMin,this.customerMax));
+  store: 'Seattle',
+  customerMin: 23,
+  customerMax: 65,
+  customerAverage: 6.3,
+  //Customer/sales per hour//
+  hourlySales: function(){
+    var customersPerHour = [];
+    var salesPerCustomer = [];
+    for (i = 0; i < hours.length; i++){
+      customersPerHour.push(getRandomInt(this.customerMin,this.customerMax));
     }
-    for (i=0; i<hours.length; i++){
-        salesPerCustomer.push(customersPerHour[i] * this.customerAverage);
+    for (i = 0; i < hours.length; i++){
+      salesPerCustomer.push(customersPerHour[i] * this.customerAverage);
     }
     console.log(customersPerHour);
     console.log(salesPerCustomer);
-    }
-}
+  },
+};
 seattleStore.hourlySales();
