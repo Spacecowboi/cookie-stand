7fb304fe4f311727a4277002bd6fc6271a3ac8c0
spacecowboi <cunningham2192@gmail.com>
2020-01-14 09:29:09 -0800

    forgot to put collab credits, adjusted

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //Open hours
+//Collaborated with Alex pena 
 var hours = [‘6am’,‘7am’,‘8am’,‘9am’,‘10am’,‘11am’,‘12pm’,‘1pm’,‘2pm’,‘3pm’,‘4pm’,‘5pm’,‘6pm’,‘7pm’];
 //RNGESUS//
 function getRandomInt(customerMin, customerMax) {
